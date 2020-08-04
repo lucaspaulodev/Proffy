6a4c96b3f95ae06552bc50572fe2ddbb2bf0ea11
Lucas Paulo <lucaspaulodev@gmail.com>
2020-08-04 16:48:28 -0300

    Creating new classes in database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/users', (request, response) => {
-    return response.json({
-        name: "Lucas",
-        age: 21,
-    })
-})
+app.use(routes)
 
 app.listen(3333)
