675ed26d4949b4e16a171f172af75a6d170c3885
Lucas Paulo <lucaspaulodev@gmail.com>
2020-08-04 15:09:35 -0300

    Connecting with Sqlite

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello World')
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/users', (request, response) => {
+    return response.json({
+        name: "Lucas",
+        age: 21,
+    })
+})
+
+app.listen(3333)
